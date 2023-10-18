5371111880005b9138fcfad50ba447ef9061e6d0
ishmaelquinteros <113136817+ishmaelquinteros@users.noreply.github.com>
2023-10-18 12:01:10 -0300

    Creado logo y categorias

--- src/componentes/Navbar/NavBar.jsx
+++ src/componentes/Navbar/NavBar.jsx
@@ -1,19 +1,21 @@
 import CartWidget from '../CartWidget/CartWidget.jsx';
 import './Navbar.css';
+import fototitulo from '../../foto-calzados-1.jpg';
 
 const NavBar = () => {
     return (
     <div className='NavContainer'>
         <div className='titulo-container'>
+            <img alt='foto nav' src={fototitulo} className='titulo-imagen'></img>
             <h1 className='titulo-principal'>Feria calzados niños</h1>
         </div>
         <div className='categorias-container'>
             <h2 className='categorias-titulo'>Categorias</h2>
             <ul className='categorias-lista'>
-              <li>Zapatillas</li> 
-              <li>Zapatos</li>
-              <li>Sandalias</li> 
-            </ul>;
+              <li className='categorias-lista-option'>Zapatillas</li> 
+              <li className='categorias-lista-option'>Zapatos</li>
+              <li className='categorias-lista-option'>Sandalias</li> 
+            </ul>
         </div>
     <CartWidget />
     </div>
